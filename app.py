25b0c9b8db9a445912056937e9c5487a8c6ae809
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-05-28 19:31:47 +0000

    Added login and register HTML templates and routes to them.

--- app.py
+++ app.py
@@ -7,15 +7,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # Render index(home) page as landing page
     return render_template("index.html")
 
 @app.route("/specs")
 def specs():
+    #Render specs(recipes) page
     return render_template("specs.html")
 
 @app.route("/contact")
 def contact():
+    #Render contact page
      return render_template("contact.html")
+
+
+@app.route("/login")
+def login():
+    #Render login page
+     return render_template("login.html")
+
+@app.route("/register")
+def register():
+    #Render register page
+     return render_template("register.html")
 
 
 if __name__ == "__main__":
