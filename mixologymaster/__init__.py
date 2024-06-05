43587fdddd6200091693e1c35ab45c14433bc2f6
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-06-05 21:41:13 +0000

    Revert "Added a link to CI Database."
    This reverts commit 92b799d33430de1b9696654ee178e1c7a210e01c.

--- mixologymaster/__init__.py
+++ mixologymaster/__init__.py
@@ -13,10 +13,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-     uri = os.environ.get("DATABASE_URL")
-     if uri.startswith("postgres://"):
-         uri = uri.replace("postgres://", "postgresql://", 1)
-     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
