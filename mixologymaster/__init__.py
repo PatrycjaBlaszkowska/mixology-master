1af76bfa284e3db08c2867549fc8d642858c89f7
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-06-07 12:14:50 +0000

    Amended requirements.txt.

--- mixologymaster/__init__.py
+++ mixologymaster/__init__.py
@@ -6,17 +6,17 @@ from flask_bcrypt import Bcrypt
 if os.path.exists("env.py"):
     import env  
 
-
 app = Flask(__name__)
+
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
+         uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
     
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
