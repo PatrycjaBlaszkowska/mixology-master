3e6a4e574e74f43d4c680b7927732c368103cfac
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-06-10 19:51:52 +0000

    Tidied-up html across all pages. Added some comments too.

---
+++ mixologymaster/static/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const search = document.getElementById('search');
+    const cocktails = document.querySelectorAll('.cocktail-card');
+    const notFound = document.getElementById('not-found');
+
+    searchInput.addEventListener('input', function() {
+        const searchValue = this.value.trim().toLowerCase();
+        let hasResults = false;
+
+        cocktails.forEach(function(card) {
+            const cocktailName = card.querySelector('.cocktail-name').textContent.trim().toLowerCase();
+            const cocktailCategory = card.querySelector('.cocktail-category').nextSibling.textContent.trim().toLowerCase();
+
+            const matchesSearch = cocktailName.includes(searchValue) || cocktailCategory.includes(searchValue)
+
+            if (matchesSearch) {
+                card.style.display = 'block';
+                hasResults = true;
+            } else {
+                card.style.display = 'none';
+            }
+        });
+
+        notFound.style.display = hasResults ? 'none' : 'block';
+    });
+});
